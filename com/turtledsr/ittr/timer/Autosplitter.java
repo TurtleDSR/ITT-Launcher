9a11b2d492818128cd99ea9182d3a4884933f740
TurtleDSR <Turtleman42007@gmail.com>
2026-01-24 22:46:23 -0500

    autosplitter complete

--- com/turtledsr/ittr/timer/Autosplitter.java
+++ com/turtledsr/ittr/timer/Autosplitter.java
@@ -9,35 +9,191 @@ import com.sun.jna.platform.win32.Kernel32;
 import com.sun.jna.platform.win32.WinNT.HANDLE;
 import com.turtledsr.ittr.process.Process;
 
+/*
+Emulates the autosplitter written by Lemuura (without debug or settings)
 
+Uses the JNA library for memory handling
+Connects to the Livesplit TCP socket to control the timer
+
+Assumes latest version of the game
+
+*HUGE credit to Lemuura*
+*/
 public final class Autosplitter {
   public static final int PROCESS_VM_READ = 0x0010;
 
   private static Kernel32 kernel32 = Native.load("kernel32", Kernel32.class);
-  private static HANDLE process;
-  private static Pointer basePointer;
+  public static HANDLE process;
 
+  private static boolean started = false;
   private static boolean running = false;
 
   private static final long baseOffset = 0x07A07A60; //"ItTakesTwo.exe", 0x07A07A60
   private static long baseAddress;
+  private static Pointer basePointer;
 
-  private static boolean isLoading;            //"ItTakesTwo.exe", 0x07A07A60, 0x180, 0x2b0, 0x0, 0x458, 0xf9;
+  //STATE
+  private static boolean isLoading = false;         //"ItTakesTwo.exe", 0x07A07A60, 0x180, 0x2b0, 0x0, 0x458, 0xf9;
+  private static boolean isLoadingOld = false;
   private static final long[] isLoadingPath = {0x180, 0x2b0, 0x0, 0x458, 0xf9};
-	private static String levelString;           //"ItTakesTwo.exe", 0x07A07A60, 0x180, 0x368, 0x8, 0x1b8, 0x0;
+	private static String levelString = "";           //"ItTakesTwo.exe", 0x07A07A60, 0x180, 0x368, 0x8, 0x1b8, 0x0;
+  private static String levelStringOld = "";
   private static final long[] levelStringPath = {0x180, 0x368, 0x8, 0x1b8, 0x0};
-	private static String checkpointString;      //"ItTakesTwo.exe", 0x07A07A60, 0x180, 0x368, 0x8, 0x1d8, 0x0;
+	private static String checkpointString = "";      //"ItTakesTwo.exe", 0x07A07A60, 0x180, 0x368, 0x8, 0x1d8, 0x0;
+  private static String checkpointStringOld = "";
   private static final long[] checkpointStringPath = {0x180, 0x368, 0x8, 0x1d8, 0x0};
-	private static String chapterString;         //"ItTakesTwo.exe", 0x07A07A60, 0x180, 0x368, 0x8, 0x1e8, 0x0;
+	private static String chapterString = "";         //"ItTakesTwo.exe", 0x07A07A60, 0x180, 0x368, 0x8, 0x1e8, 0x0;
+  private static String chapterStringOld = "";
   private static final long[] chapterStringPath = {0x180, 0x368, 0x8, 0x1e8, 0x0};
-	private static String subchapterString;      //"ItTakesTwo.exe", 0x07A07A60, 0x180, 0x368, 0x8, 0x1f8, 0x0;
+	private static String subchapterString = "";      //"ItTakesTwo.exe", 0x07A07A60, 0x180, 0x368, 0x8, 0x1f8, 0x0;
+  private static String subchapterStringOld = "";
   private static final long[] subchapterStringPath = {0x180, 0x368, 0x8, 0x1f8, 0x0};
-	private static String cutsceneString;        //"ItTakesTwo.exe", 0x07A07A60, 0x180, 0x2b0, 0x0, 0x390, 0x2a0, 0x788, 0x0;
+	private static String cutsceneString = "";        //"ItTakesTwo.exe", 0x07A07A60, 0x180, 0x2b0, 0x0, 0x390, 0x2a0, 0x788, 0x0;
+  private static String cutsceneStringOld = "";
   private static final long[] cutsceneStringPath = {0x180, 0x2b0, 0x0, 0x390, 0x2a0, 0x788, 0x0};
-	private static boolean skippable;               //"ItTakesTwo.exe", 0x07A07A60, 0x180, 0x2b0, 0x0, 0x390, 0x318;
+	private static boolean skippable = false;         //"ItTakesTwo.exe", 0x07A07A60, 0x180, 0x2b0, 0x0, 0x390, 0x318;
+  private static boolean skippableOld = false;
   private static final long[] skippablePath = {0x180, 0x2b0, 0x0, 0x390, 0x318};
 
-  public static void start() throws Exception {
+  //VARS
+  private static String lastCutscene = "";
+  private static String lastCutsceneOld = "";
+
+  private static int splitIndex = -1;
+  private static int splitIndexOld = -1;
+
+  private static boolean splitNextLoad = false;
+
+  private static boolean igtPaused = false;
+
+  private static boolean timerSplit = false;
+
+
+  //SPLITDATA
+  private static final String[] levelSplits = {
+		"/Game/Maps/Shed/Vacuum/Vacuum_BP",
+		"/Game/Maps/Shed/Main/Main_Hammernails_BP",
+		"/Game/Maps/Shed/Main/Main_Grindsection_BP",
+		"/Game/Maps/RealWorld/RealWorld_Shed_StarGazing_Meet_BP",
+		"/Game/Maps/Tree/SquirrelHome/SquirrelHome_BP_Mech",
+		"/Game/Maps/Tree/WaspNest/WaspsNest_BP",
+		"/Game/Maps/Tree/Boss/WaspQueenBoss_BP",
+		"/Game/Maps/Tree/Escape/Escape_BP",
+		"/Game/Maps/PlayRoom/Spacestation/Spacestation_Hub_BP",
+		"/Game/Maps/PlayRoom/Hopscotch/Hopscotch_BP",
+		"/Game/Maps/PlayRoom/Goldberg/Goldberg_Trainstation_BP",
+		"/Game/Maps/PlayRoom/Goldberg/Goldberg_Pirate_BP",
+		"/Game/Maps/PlayRoom/Courtyard/Castle_Courtyard_BP",
+		"/Game/Maps/PlayRoom/Dungeon/Castle_Dungeon_BP",
+		"/Game/Maps/PlayRoom/Shelf/Shelf_BP",
+		"/Game/Maps/RealWorld/RealWorld_RoseRoom_Bed_Tears_BP",
+		"/Game/Maps/Clockwork/LowerTower/Clockwork_ClockTowerLower_CrushingTrapRoom_BP",
+		"/Game/Maps/Clockwork/UpperTower/Clockwork_ClockTowerLastBoss_BP",
+		"/Game/Maps/SnowGlobe/Town/SnowGlobe_Town_BP",
+		"/Game/Maps/SnowGlobe/Lake/Snowglobe_Lake_BP",
+		//"/Game/Maps/SnowGlobe/Mountain/SnowGlobe_Mountain_BP",
+		"/Game/Maps/Garden/Shrubbery/Garden_Shrubbery_BP",
+		"/Game/Maps/Garden/MoleTunnels/Garden_MoleTunnels_Stealth_BP",
+		"/Game/Maps/Garden/FrogPond/Garden_FrogPond_BP",
+		"/Game/Maps/Garden/Greenhouse/Garden_Greenhouse_BP",
+		"/Game/Maps/Music/Nightclub/Music_Nightclub_BP",
+		"/Game/Maps/Music/Backstage/Music_Backstage_Tutorial_BP",
+		"/Game/Maps/Music/Classic/Music_Classic_Organ_BP",
+		"/Game/Maps/Music/Ending/Music_Ending_BP",
+
+		"SnowGlobe_Mountain",
+
+    //INTERMEDIARIES - ASSUMED
+    "/Game/Maps/Shed/Awakening/Awakening_BP",
+		"/Game/Maps/Tree/Approach/Approach_BP",
+		"/Game/Maps/PlayRoom/PillowFort/PillowFort_BP",
+		"/Game/Maps/Clockwork/Outside/Clockwork_Tutorial_BP",
+		"/Game/Maps/SnowGlobe/Forest/SnowGlobe_Forest_BP",
+		"/Game/Maps/Garden/VegetablePatch/Garden_VegetablePatch_BP",
+		"/Game/Maps/Music/ConcertHall/Music_ConcertHall_BP"
+	};
+
+  private static final String[] cutsceneSplits = {
+		"CS_RealWorld_House_LivingRoom_Headache",
+		"CS_ClockWork_UpperTower_EndingRewind",
+		"CS_Garden_GreenHouse_BossRoom_Outro",
+	};
+
+  private static final String[] postCutsceneSplits = {
+		"CS_PlayRoom_DinoLand_DinoCrash_Intro",
+		"CS_PlayRoom_Circus_Balloon_Intro",
+	};
+
+  private static final String[] oldLevelSplits = {
+		"/Game/Maps/Shed/Awakening/Awakening_BP",
+		"/Game/Maps/Shed/Vacuum/Vacuum_BP",
+		"/Game/Maps/Shed/Main/Main_Hammernails_BP",
+		"/Game/Maps/Shed/Main/Main_Bossfight_BP",
+		"/Game/Maps/Shed/Main/Main_Grindsection_BP",
+		"/Game/Maps/RealWorld/RealWorld_Shed_StarGazing_Meet_BP",
+		"/Game/Maps/Tree/SquirreTurf/SquirrelTurf_WarRoom_BP",
+		"/Game/Maps/Tree/SquirrelHome/SquirrelHome_BP_Mech",
+		"/Game/Maps/Tree/WaspNest/WaspsNest_BeetleRide_BP",
+		"/Game/Maps/Tree/SquirreTurf/SquirrelTurf_Flight_BP",
+		"/Game/Maps/RealWorld/RealWorld_Exterior_Roof_Crash_BP",
+		"/Game/Maps/PlayRoom/PillowFort/PillowFort_BP",
+		"/Game/Maps/RealWorld/Realworld_SpaceStation_Bossfight_BeamOut_BP",
+		"/Game/Maps/PlayRoom/Hopscotch/Kaleidoscope_BP",
+		"/Game/Maps/PlayRoom/Goldberg/Goldberg_Trainstation_BP",
+		"/Game/Maps/PlayRoom/Goldberg/Goldberg_Dinoland_BP",
+		"/Game/Maps/PlayRoom/Goldberg/Goldberg_Pirate_BP",
+		"/Game/Maps/PlayRoom/Goldberg/Goldberg_Circus_BP",
+		"/Game/Maps/PlayRoom/Courtyard/Castle_Courtyard_BP",
+		"/Game/Maps/PlayRoom/Chessboard/Castle_Chessboard_BP",
+		"/Game/Maps/PlayRoom/Shelf/Shelf_BP",
+		"/Game/Maps/TherapyRoom/TherapyRoom_Session_Theme_Time_BP",
+		"/Game/Maps/Clockwork/Outside/Clockwork_ClockTowerCourtyard_BP",
+		"/Game/Maps/Clockwork/Outside/Clockwork_Forest_BP",
+		"/Game/Maps/Clockwork/LowerTower/Clockwork_ClockTowerLower_CuckooBirdRoom_BP",
+		"/Game/Maps/Clockwork/LowerTower/Clockwork_ClockTowerLower_EvilBirdRoom_BP",
+		"/Game/Maps/TherapyRoom/TherapyRoom_Session_Theme_Attraction_BP",
+		"/Game/Maps/SnowGlobe/Forest/SnowGlobe_Forest_BP",
+		"/Game/Maps/SnowGlobe/Forest/SnowGlobe_Forest_TownGate_BP",
+		"/Game/Maps/SnowGlobe/Town/SnowGlobe_Town_BobSled",
+		//"/Game/Maps/SnowGlobe/Lake/Snowglobe_Lake_BP",
+		"/Game/Maps/TherapyRoom/TherapyRoom_Session_Theme_Garden_BP",
+		"/Game/Maps/Garden/VegetablePatch/Garden_VegetablePatch_BP",
+		"/Game/Maps/Garden/Shrubbery/Garden_Shrubbery_SecondCombat_BP",
+		"/Game/Maps/Garden/Shrubbery/Garden_Shrubbery_BP",
+		"/Game/Maps/Garden/MoleTunnels/Garden_MoleTunnels_Chase_BP",
+		"/Game/Maps/Garden/MoleTunnels/Garden_MoleTunnels_Stealth_BP",
+		"/Game/Maps/Garden/FrogPond/Garden_FrogPond_BP",
+		"/Game/Maps/TherapyRoom/TherapyRoom_Session_Theme_Music_BP",
+		"/Game/Maps/Music/ConcertHall/Music_ConcertHall_BP",
+		"/Game/Maps/Music/Nightclub/Music_Nightclub_BP",
+
+		"/Game/Maps/Tree/WaspNest/WaspsNest_BP",
+		"/Game/Maps/Tree/WaspNest/WaspsNest_Swarm_BP",
+		"/Game/Maps/Tree/Boat/Tree_Boat_BP",
+		"/Game/Maps/Tree/Darkroom/Tree_Darkroom_BP",
+		"/Game/Maps/PlayRoom/Hopscotch/Hopscotch_BP",
+		"/Game/Maps/PlayRoom/Hopscotch/VoidWorld_BP",
+		"/Game/Maps/PlayRoom/Dungeon/Castle_Dungeon_BP",
+		"/Game/Maps/PlayRoom/Dungeon/Castle_Dungeon_Charger_BP",
+
+		"SnowGlobe_Lake",
+	};
+
+  private static final String[] startLevels = {
+		"/Game/Maps/Shed/Awakening/Awakening_BP",
+		"/Game/Maps/Tree/Approach/Approach_BP",
+		"/Game/Maps/PlayRoom/PillowFort/PillowFort_BP",
+		"/Game/Maps/Clockwork/Outside/Clockwork_Tutorial_BP",
+		"/Game/Maps/SnowGlobe/Forest/SnowGlobe_Forest_BP",
+		"/Game/Maps/Garden/VegetablePatch/Garden_VegetablePatch_BP",
+		"/Game/Maps/Music/ConcertHall/Music_ConcertHall_BP"
+	};
+
+  private static final String[] checkpointSplits = {"TerraceProposalCutscene"};
+
+  private static final String[] nextLoadSplits = {};
+
+  public static void run() throws Exception {
     OptionalInt pid = Process.getProcessPID("ItTakesTwo");
     if(!pid.isPresent()) {
       throw new Exception("Process Not Found");
@@ -56,17 +212,66 @@ public final class Autosplitter {
   }
 
   public static void stop() {
+    System.err.println("AUTOSPLITTER");
     kernel32.CloseHandle(process);
     running = false;
+
+    process = null;
+
+    if(!igtPaused) {
+      TimerHandler.pauseGameTime();
+      igtPaused = true;
+    }
   }
 
   public static void tick() {
+    if(Process.getProcessPID("ItTakesTwo").isEmpty()) { //check if process closed
+      stop();
+      return;
+    }
+
+    initTick();
+
     if(running) {
-      initTick();
+      if(started) {
+        if(update()) {
+          if(isLoading() != igtPaused) {
+            igtPaused = !igtPaused;
+            if(igtPaused) {
+              TimerHandler.pauseGameTime();
+            } else {
+              TimerHandler.unpauseGameTime();
+            }
+          }
+          if(reset()) {
+            TimerHandler.reset();
+          } else {
+            if(split()) {
+              TimerHandler.split();
+              timerSplit = true;
+            }
+          }
+        }
+      } else {
+        if(start()) {
+          TimerHandler.start();
+          started = true;
+        }
+      }
     }
   }
 
   public static void initTick() {
+    //UPDATE OLD STATE
+    isLoadingOld = isLoading;
+    levelStringOld = levelString;
+    checkpointStringOld = checkpointString;
+    chapterStringOld = chapterString;
+    subchapterStringOld = subchapterString;
+    cutsceneStringOld = cutsceneString;
+    skippableOld = skippable;
+
+    //UPDATE CURRENT STATE
     //ISLOADING
     Memory mIsLoading = followPath(isLoadingPath, 1);
     if(mIsLoading == null) {
@@ -75,7 +280,7 @@ public final class Autosplitter {
       isLoading = mIsLoading.getByte(0) != 0;
     }
     //System.out.println(isLoading);
-    
+
     //LEVELSTRING
     Memory mLevelString = followPath(levelStringPath, 128);
     if(mLevelString == null) {
@@ -131,9 +336,117 @@ public final class Autosplitter {
     //System.out.println(skippable);
   }
 
-  //be very careful what you pass into here
-  private static Memory followPath(long[] path, int size) {
-    Memory buffer = new Memory(8); 
+  //TIMER CONTROL
+
+  private static boolean update() {
+    splitIndexOld = splitIndex;
+    splitIndex = TimerHandler.getCurrentIndex();
+
+    if(!timerSplit && splitIndexOld != splitIndex) {
+      if(splitIndex == -1) {
+        //player reset [HANDLE FORFEIT]
+        started = false;
+      }
+
+      TimerHandler.unsplit();
+      return false;
+    }
+
+    if(splitIndex == -1) {
+      if(timerSplit) {
+        //player completed [HANDLE COMPLETION]
+      }
+    }
+
+    lastCutsceneOld = lastCutscene;
+    if (cutsceneString == null) lastCutscene = "null";
+	  if (cutsceneString != null && cutsceneString.length() > 1) lastCutscene = cutsceneString;
+    return true;
+  }
+
+  @Deprecated
+  private static boolean reset() { //unimplemented
+    return false;
+  }
+
+  private static boolean split() {
+    if(splitNextLoad && isLoading && !isLoadingOld) {
+	    splitNextLoad = false;
+		  return true;
+	  }
+
+	  if(!levelStringOld.equals(levelString)) {
+		  if(contains(levelSplits, levelString) && contains(oldLevelSplits, levelStringOld)) {
+			  return true;
+      }
+    }
+
+	  if(!subchapterStringOld.equals(subchapterString)) {
+	  	if(contains(levelSplits, subchapterString) && contains(oldLevelSplits, subchapterStringOld)) return true;
+	  }
+
+	  if(!checkpointStringOld.equals(checkpointString)) {
+	  	if (contains(checkpointSplits, checkpointString)) {
+	  		return true;
+      }
+
+	  	if (contains(nextLoadSplits, checkpointString)) {
+	  		splitNextLoad = true;
+      }
+	  }
+
+	  if (!lastCutsceneOld.equals(lastCutscene)) {
+	  	if (lastCutscene.equals("CS_Music_Attic_Stage_ClimacticKiss")) {// Ending split, always active.
+	  		return true;
+      }
+
+	  	if (contains(cutsceneSplits, lastCutscene)) {
+	  		return true;
+      }
+
+	  	if (contains(postCutsceneSplits, lastCutsceneOld))
+	  		return true;
+
+	  	if (contains(nextLoadSplits, lastCutscene)) {
+	  		splitNextLoad = true;
+      }
+	  }
+    return false;
+  }
+
+  private static boolean start() {
+    if(!isLoading) {
+      return false;
+    }
+    
+    if (contains(startLevels, levelString)) {
+			return true;
+    }
+    return false;
+  }
+
+  private static boolean isLoading() {
+    if (isLoading)
+		  return true;
+	  if (levelString.equals("/Game/Maps/Main/Menu_BP"))
+	  	return true;
+	  if (levelString == null) //should only be null when game is exited
+	  	return true;
+	  if (!levelString.equals("/Game/Maps/Main/Menu_BP"))
+	  	return false;
+    return true;
+  }
+
+  private static boolean contains(Object[] arr, Object obj) {
+    for (Object object : arr) {
+      if(object.equals(obj)) return true;
+    }
+
+    return false;
+  }
+
+  private static Memory followPath(long[] path, int size) { //returns null if pointer path is invalid
+    Memory buffer = new Memory(8);
     Pointer p = basePointer.share(baseOffset);
     
     if (!kernel32.ReadProcessMemory(process, p, buffer, 8, null)) {
--- com/turtledsr/ittr/timer/TimerHandler.java
+++ com/turtledsr/ittr/timer/TimerHandler.java
@@ -11,6 +11,8 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public final class TimerHandler {
+  public static boolean awaitingReconnection = false; 
+
   public static final int reconnectionInterval = 500;
   public static Socket socket;
 
@@ -22,8 +24,15 @@ public final class TimerHandler {
   public static void connect() {
     try{
       socket = new Socket("localhost", 16834);
+      socket.setSoTimeout(1000);
+
       out = new PrintWriter(socket.getOutputStream(), true);
       in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+      out.print("switchto gametime\r\n"); //force IGT
+      out.flush();
+
+      awaitingReconnection = false;
     } catch(Exception e) {
       System.err.println(e.getMessage());
       scheduleReconnect(reconnectionInterval);
@@ -31,11 +40,14 @@ public final class TimerHandler {
   }
 
   public static void scheduleReconnect(int time) {
+    awaitingReconnection = true;
     executor.schedule(new Runnable() {
       @Override
       public void run() {
         try{
           if(socket != null) {
+            in = null;
+            out = null;
             socket.close();
           }
           connect();
@@ -47,18 +59,99 @@ public final class TimerHandler {
     System.out.println("Scheduling reconnection attempt in " + time + "ms");
   }
 
-  public static String GetCurrentSplitName() {
-    out.print("getcurrentsplitname\r\n");
+  public static int getCurrentIndex() {
+    if(out == null) return -1;
+
+    out.print("getsplitindex\r\n");
     out.flush();
 
-    String split = "ERR";
+    try{Thread.sleep(10);} catch(InterruptedException e) {}
+
+    int split = -1;
 
     try{
-      split = in.readLine();
+      split = Integer.parseInt(in.readLine());
     } catch (Exception e) {
       System.err.println(e.getMessage());
     }
 
     return split;
+  }
+
+  public static void split() {
+    if(out == null) return;
+
+    System.out.println("SPLITTING");
+
+    out.print("split\r\n");
+    out.flush();
+
+    try{Thread.sleep(10);} catch(InterruptedException e) {}
+  }
+
+  public static void unsplit() {
+    if(out == null) return;
+
+    System.out.println("UNSPLITTING");
+
+    out.print("unsplit\r\n");
+    out.flush();
+
+    try{Thread.sleep(10);} catch(InterruptedException e) {}
+  }
+
+  public static void start() {
+    if(out == null) return;
+
+    System.out.println("STARTING");
+
+    out.print("starttimer\r\n");
+    out.flush();
+
+    try{Thread.sleep(10);} catch(InterruptedException e) {}
+  }
+
+  public static void pause() {
+    if(out == null) return;
+
+    System.out.println("PAUSING");
+
+    out.print("pause\r\n");
+    out.flush();
+
+    try{Thread.sleep(10);} catch(InterruptedException e) {}
+  }
+
+  public static void pauseGameTime() {
+    if(out == null) return;
+
+    System.out.println("PAUSING GAMETIME");
+
+    out.print("pausegametime\r\n");
+    out.flush();
+
+    try{Thread.sleep(10);} catch(InterruptedException e) {}
+  }
+
+  public static void unpauseGameTime() {
+    if(out == null) return;
+
+    System.out.println("UNPAUSING GAMETIME");
+
+    out.print("unpausegametime\r\n");
+    out.flush();
+
+    try{Thread.sleep(10);} catch(InterruptedException e) {}
+  }
+
+  public static void reset() {
+    if(out == null) return;
+
+    System.out.println("RESETTING");
+
+    out.print("reset\r\n");
+    out.flush();
+
+    try{Thread.sleep(10);} catch(InterruptedException e) {}
   }
 }
